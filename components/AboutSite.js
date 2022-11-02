7971f6841d1592a307747de7bb1c45ecf6aa60bf
bjabot12 <bjarte1997@hotmail.com>
2022-11-02 20:08:41 +0100

    done done

--- components/AboutSite.js
+++ components/AboutSite.js
@@ -2,7 +2,7 @@ export default function AboutSite() {
   return (
     <div className="aboutSite">
         <h4>About this blog</h4>
-        <p>Hi. My name is Bjarte Botnevik and I work as a Data Engineer. On this blog I will write a post on the small
+        <p>Hi. My name is Bjarte Botnevik and I work as a Data Engineer. On this blog I will write posts on
              hobby projects. This includes Kaggle competitions, small electronics projects and other topics that interests
              me. It is intended to be used as a blog/portfolio for my own enjoyment, but I will share the post
              in case others find it interesting.
